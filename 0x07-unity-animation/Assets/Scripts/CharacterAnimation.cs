6541ab65ac05ae4075e9a6134c6d882c601fe9fa
JuanJoseGomezR <juangomez0149@gmail.com>
2021-02-25 10:54:54 -0500

    Uploading

---
+++ 0x07-unity-animation/Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+}
---
+++ 0x07-unity-animation/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public Canvas PauseCanvas;
+
+    public bool PausedGame;
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("escape");
+
+            if (PausedGame)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+
+            }
+        }
+    }
+    public void Pause()
+    {
+
+        PauseCanvas.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+        PausedGame = true;
+    }
+
+    public void Resume()
+    {
+        PauseCanvas.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+        PausedGame = false;
+    }
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Options()
+    {
+        Time.timeScale = 1f;
+        PlayerPrefs.SetString("lastScene", SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("OptionsMenu");
+    }
+}
