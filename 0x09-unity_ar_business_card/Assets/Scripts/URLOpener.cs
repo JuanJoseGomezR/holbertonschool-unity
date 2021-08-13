b5a68c0c7a9ef3609577983096fcfa6ea6975a1e
JuanJoseGomezR <juangomez0149@gmail.com>
2021-08-12 22:06:35 -0500

    ARcard

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/URLOpener.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Mime;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URLOpener : MonoBehaviour
+{
+    public string Url;
+
+    public void Open()
+    {
+        Application.OpenURL(Url);
+    }
+}
