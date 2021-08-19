5b31d45f90617a351e6f9e634e621fb38c21d6f2
JuanJoseGomezR <juangomez0149@gmail.com>
2021-08-19 16:17:20 -0500

    Commit

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/SFX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SFX : MonoBehaviour
+{
+    public AudioSource audio;
+
+    public AudioClip sound;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void playSound()
+    {
+        audio.PlayOneShot(sound);
+    }
+}
